c2f13c963e9fb7be43c8eaec850c3c54adf0093f
Kyle <kyletrusler@gmail.com>
2023-05-14 01:21:33 +0200

    feat: :thread: Add the other statuses

--- src/actions/discord.ts
+++ src/actions/discord.ts
@@ -4,6 +4,7 @@ import hash from "hash-emoji";
 import { nodeHexToID } from "./utils";
 import { ethers } from "ethers";
 import { formatDistance } from "date-fns";
+import { TransactionEvent } from "@tenderly/actions";
 
 export const getEmojiAddress = (address: string) => {
   return `${hash(address)} ${address.slice(0, 6)}...${address.slice(-4)}`;
@@ -16,8 +17,6 @@ export const getEmojiNodeId = (address: string) => {
 };
 
 const commonFields = (nodeId: string, minipoolOwner: string) => {
-  console.log(minipoolOwner);
-
   return [
     {
       name: "🧑‍✈️ pilot",
@@ -36,6 +35,7 @@ const commonFields = (nodeId: string, minipoolOwner: string) => {
   ];
 };
 export const MINIPOOL_PRELAUNCH_TEMPLATE = (
+  transactionEvent: TransactionEvent,
   nodeId: string,
   minipoolOwner: string
 ) => {
@@ -44,13 +44,17 @@ export const MINIPOOL_PRELAUNCH_TEMPLATE = (
     embeds: [
       {
         title: "A minipool is getting ready.",
+        url: `https://snowtrace.io/tx/${transactionEvent.transactionHash}`,
         fields: [...commonFields(nodeId, minipoolOwner)],
+        color: 0x5d43ef,
+        description: "[prelaunch]",
       },
     ],
   };
 };
 
 export const MINIPOOL_LAUNCH_TEMPLATE = (
+  transactionEvent: TransactionEvent,
   nodeId: string,
   minipoolOwner: string,
   duration: string
@@ -61,6 +65,9 @@ export const MINIPOOL_LAUNCH_TEMPLATE = (
     embeds: [
       {
         title: "Minipool is ready!",
+        url: `https://snowtrace.io/tx/${transactionEvent.transactionHash}`,
+        color: 0x5d43ef,
+        description: "[launched]",
         fields: [
           ...commonFields(nodeId, minipoolOwner),
           {
@@ -80,12 +87,19 @@ export const MINIPOOL_LAUNCH_TEMPLATE = (
   };
 };
 
-export const MINIPOOL_STAKING_TEMPLATE = (nodeId: string, owner: string) => {
+export const MINIPOOL_STAKING_TEMPLATE = (
+  transactionEvent: TransactionEvent,
+  nodeId: string,
+  owner: string
+) => {
   return {
     username: "GoGoObserver 🎈🔭",
     embeds: [
       {
         title: "We have liftoff!",
+        url: `https://snowtrace.io/tx/${transactionEvent.transactionHash}`,
+        color: 0x5d43ef,
+        description: "[staking]",
         fields: [...commonFields(nodeId, owner)],
       },
     ],
@@ -93,6 +107,7 @@ export const MINIPOOL_STAKING_TEMPLATE = (nodeId: string, owner: string) => {
 };
 
 export const MINIPOOL_WITHDRAWABLE_TEMPLATE = (
+  transactionEvent: TransactionEvent,
   nodeId: string,
   owner: string
 ) => {
@@ -101,18 +116,85 @@ export const MINIPOOL_WITHDRAWABLE_TEMPLATE = (
     embeds: [
       {
         title: "Minipool is ready to be withdrawn!",
+        url: `https://snowtrace.io/tx/${transactionEvent.transactionHash}`,
+        description: "[withdrawable]",
+        color: 0x5d43ef,
         fields: [...commonFields(nodeId, owner)],
       },
     ],
   };
 };
 
-export const MINIPOOL_RESTAKE_TEMPLATE = (nodeId: string, owner: string) => {
+export const MINIPOOL_FINISHED_TEMPLATE = (
+  transactionEvent: TransactionEvent,
+  nodeId: string,
+  owner: string
+) => {
+  return {
+    username: "GoGoObserver 🎈🔭",
+    embeds: [
+      {
+        title: "Minipool has finished it's flight.",
+        url: `https://snowtrace.io/tx/${transactionEvent.transactionHash}`,
+        description: "[finished]",
+        color: 0x5d43ef,
+        fields: [...commonFields(nodeId, owner)],
+      },
+    ],
+  };
+};
+
+export const MINIPOOL_CANCELED_TEMPLATE = (
+  transactionEvent: TransactionEvent,
+  nodeId: string,
+  owner: string
+) => {
+  return {
+    username: "GoGoObserver 🎈🔭",
+    embeds: [
+      {
+        title: "Minipool is grounded.",
+        url: `https://snowtrace.io/tx/${transactionEvent.transactionHash}`,
+        description: "[canceled]",
+        color: 0x5d43ef,
+        fields: [...commonFields(nodeId, owner)],
+      },
+    ],
+  };
+};
+
+export const MINIPOOL_ERROR_TEMPLATE = (
+  transactionEvent: TransactionEvent,
+  nodeId: string,
+  owner: string
+) => {
+  return {
+    username: "GoGoObserver 🎈🔭",
+    embeds: [
+      {
+        title: "Minipool has crashed.",
+        url: `https://snowtrace.io/tx/${transactionEvent.transactionHash}`,
+        description: "[error]",
+        color: 0x5d43ef,
+        fields: [...commonFields(nodeId, owner)],
+      },
+    ],
+  };
+};
+
+export const MINIPOOL_RESTAKE_TEMPLATE = (
+  transactionEvent: TransactionEvent,
+  nodeId: string,
+  owner: string
+) => {
   return {
     username: "GoGoObserver 🎈🔭",
     embeds: [
       {
         title: "Minipool has ended it's flight and restaked!",
+        url: `https://snowtrace.io/tx/${transactionEvent.transactionHash}`,
+        description: "[withdrawable] • [prelaunch] • [launched]",
+        color: 0x5d43ef,
         fields: [...commonFields(nodeId, owner)],
       },
     ],
--- src/actions/minipool.ts
+++ src/actions/minipool.ts
@@ -4,6 +4,9 @@ import {
   MINIPOOL_MANAGER_INTERFACE,
 } from "./constants";
 import {
+  MINIPOOL_CANCELED_TEMPLATE,
+  MINIPOOL_ERROR_TEMPLATE,
+  MINIPOOL_FINISHED_TEMPLATE,
   MINIPOOL_LAUNCH_TEMPLATE,
   MINIPOOL_PRELAUNCH_TEMPLATE,
   MINIPOOL_RESTAKE_TEMPLATE,
@@ -35,6 +38,7 @@ export const minipoolStatusChange = async (context: Context, event: Event) => {
     "https://api.avax.network/ext/bc/C/rpc"
   );
   const transactionEvent = event as TransactionEvent;
+  console.log(transactionEvent)
   const statusChangedEvents = await getMatchingEvents<MinipoolStatusChanged>(
     transactionEvent,
     MINIPOOL_MANAGER_INTERFACE,
@@ -47,31 +51,59 @@ export const minipoolStatusChange = async (context: Context, event: Event) => {
     const { status, nodeID } = statusChangedEvents[0];
     switch (status.toString()) {
       case "0":
-        message = MINIPOOL_PRELAUNCH_TEMPLATE(nodeID, transactionEvent.from);
+        message = MINIPOOL_PRELAUNCH_TEMPLATE(
+          transactionEvent,
+          nodeID,
+          transactionEvent.from
+        );
         break;
       case "1": {
         const { owner, duration } = await getMinipoolDataFromNodeId(
           provider,
           nodeID
         );
-        message = MINIPOOL_LAUNCH_TEMPLATE(nodeID, owner, duration);
+        message = MINIPOOL_LAUNCH_TEMPLATE(
+          transactionEvent,
+          nodeID,
+          owner,
+          duration
+        );
         break;
       }
       case "2": {
         const { owner } = await getMinipoolDataFromNodeId(provider, nodeID);
-        message = MINIPOOL_STAKING_TEMPLATE(nodeID, owner);
+        message = MINIPOOL_STAKING_TEMPLATE(transactionEvent, nodeID, owner);
         break;
       }
       case "3": {
         const { owner } = await getMinipoolDataFromNodeId(provider, nodeID);
-        message = MINIPOOL_WITHDRAWABLE_TEMPLATE(nodeID, owner);
+        message = MINIPOOL_WITHDRAWABLE_TEMPLATE(
+          transactionEvent,
+          nodeID,
+          owner
+        );
+        break;
+      }
+      case "4": {
+        const { owner } = await getMinipoolDataFromNodeId(provider, nodeID);
+        message = MINIPOOL_ERROR_TEMPLATE(transactionEvent, nodeID, owner);
+        break;
+      }
+      case "5": {
+        const { owner } = await getMinipoolDataFromNodeId(provider, nodeID);
+        message = MINIPOOL_CANCELED_TEMPLATE(transactionEvent, nodeID, owner);
+        break;
+      }
+      case "6": {
+        const { owner } = await getMinipoolDataFromNodeId(provider, nodeID);
+        message = MINIPOOL_FINISHED_TEMPLATE(transactionEvent, nodeID, owner);
         break;
       }
     }
   } else {
     const { nodeID } = statusChangedEvents[0];
     const { owner } = await getMinipoolDataFromNodeId(provider, nodeID);
-    message = MINIPOOL_RESTAKE_TEMPLATE(nodeID, owner);
+    message = MINIPOOL_RESTAKE_TEMPLATE(transactionEvent, nodeID, owner);
   }
 
   await sendWebhook(await context.secrets.get("MANGO_WEBHOOK_URL"), message);
